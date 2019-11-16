22748a6302a98f64babd3570960cbcf0c8098b52
NotArtyom <smart.artyom@gmail.com>
2019-11-17 01:02:31 +0300

    Initial Commit. Tasks 1,2 and 4

---
+++ Task_Concat.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Task_Concat.h"
+
+void strcat(char* to, const char* from)
+{
+  for(int i = 0; i < sizeof(to);i++ ){
+    if (to[i] == '\0')
+    {
+      for (int j = 0; j < sizeof(from); j++){
+        to[i + j] = from[j];
+      }
+      return;
+    }
+  }
+}
+
+int main()
+{
+  char a[] = "asd\0";
+  char b[] = "asd\0";
+  strcat(a,b);
+  std::cout << a;
+}
---
+++ Task_Concat.h
@@ -0,0 +1,4 @@
+#ifndef TASK_CONCAT_H
+#define TASK_CONCAT_H
+
+#endif // TASK_CONCAT_H
