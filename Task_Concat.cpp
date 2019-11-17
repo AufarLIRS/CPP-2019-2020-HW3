fa89f50642411e5f6adce11f422f2581b9d32df5
NotArtyom <smart.artyom@gmail.com>
2019-11-17 18:07:03 +0300

    Clang again

--- Task_Concat.cpp
+++ Task_Concat.cpp
@@ -3,10 +3,12 @@
 
 void strcat(char* to, const char* from)
 {
-  for(int i = 0; i < sizeof(to);i++ ){
+  for (int i = 0; i < sizeof(to); i++)
+  {
     if (to[i] == '\0')
     {
-      for (int j = 0; j < sizeof(from); j++){
+      for (int j = 0; j < sizeof(from); j++)
+      {
         to[i + j] = from[j];
       }
       return;
@@ -18,6 +20,6 @@ int main()
 {
   char a[] = "asd\0";
   char b[] = "asd\0";
-  strcat(a,b);
+  strcat(a, b);
   std::cout << a;
 }
