ad28a2cbe08b6fc14d7dc79bb58e3f9e4d3309c2
frendnambe <49234020+frendnambe@users.noreply.github.com>
2019-12-23 11:38:56 +0300

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,65 @@
+KK #include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+#include <QMessageBox>
+#include <user.h>
+
+    MainWindow::MainWindow(QWidget* parent)
+  : QMainWindow(parent), ui(new Ui::MainWindow)
+{
+  ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+  delete ui;
+}
+
+std::vector<User> User::users_;
+
+void MainWindow::on_submitPushButton_clicked()
+{
+  qDebug() << "User clicked on submit button";
+
+  User::addUser(User(22, "Timofey"));
+  User::addUser(User(33, "Aufar"));
+  User::addUser(User(44, "Valery"));
+  User::addUser(User(55, "Kirill"));
+  User::addUser(User(66, "Artem"));
+  User::addUser(User(77, "Camila"));
+
+  qDebug() << "Current students' count: " << User::getCount();
+  qDebug() << "All students: ";
+  qDebug() << User::getAllNames();
+  qDebug() << "Removing user at 4 index";
+
+  User::removeUserAt(4);
+
+  qDebug() << "Current Students' Count: " << User::getCount();
+  qDebug() << "All students: ";
+  qDebug() << User::getAllNames();
+  qDebug() << "Removing user named Aufar ";
+
+  User::removeUserByName("Aufar");
+
+  qDebug() << "Current Students' Count: " << User::getCount();
+  qDebug() << "All students: ";
+  qDebug() << User::getAllNames();
+
+  // Тестируем конкатенцию слов и resize
+  /*
+  char *start = "Hello, " + '\0';
+  char *from = "world!" + '\0';
+  char *to = (char*)malloc(strlen(start) + strlen(from) + 1);   // выделяем для to место для двух слов
+  memcpy(to, start, strlen(start) + 1);                         // to = "Hello, "
+  User::strcat(to, from);                                       // to = "Hello, world!"
+  qDebug() << to;
+  */
+
+  // Тестируем конкатенцию слов и resize
+  char* to = "Hello, " + '\0';
+  char* from = "world!" + '\0';
+  to = User::resize(to, strlen(to) + 1, strlen(to) + strlen(from) + 1);  // выделяем для to место для двух слов
+  User::strcat(to, from);                                                // to = "Hello, world!"
+  qDebug() << to;
+}
---
+++ user.cpp
@@ -0,0 +1,61 @@
+#include "user.h"
+
+User::User(int age, QString name)
+{
+  this->age_ = age;
+  this->name_ = name;
+}
+
+QString User::getName() const
+{
+  return name_;
+}
+
+int User::getAge() const
+{
+  return age_;
+}
+
+size_t User::getCount()
+{
+  return users_.size();
+}
+
+void User::addUser(User user)
+{
+  users_.push_back(user);
+}
+
+void User::removeUserAt(int index)
+{
+  users_.erase(users_.begin() + index);
+}
+
+void User::removeUserByName(QString name)
+{
+  users_.erase(std::remove_if(users_.begin(), users_.end(), [&](User found) { return found.getName() == name; }),
+               users_.end());
+}
+
+std::vector<QString> User::getAllNames()
+{
+  std::vector<QString> list;
+  std::for_each(users_.begin(), users_.end(), [&](User found) { list.push_back(found.getName()); });
+  return list;
+}
+
+void User::strcat(char* to, const char* from)
+{
+  memcpy(to + strlen(to), from, strlen(from) + 1);  // + 1 для нуля
+}
+
+char* User::resize(char* str, unsigned size, unsigned new_size)
+{
+  if (new_size < size)
+    return nullptr;
+
+  char* tmp = new char[new_size];  // Выделяем память для новой строки
+  memcpy(tmp, str, size);          // Копируем старую строку в новую
+  delete[] str;                    // Удаляем память со старыми данными
+  return tmp;  // Возвращаем новую строку со скопированными данными
+}
